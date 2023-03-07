2f488efe7161c3131b4df9a709e82b8bb980838c
Andrea Scuderi <andreascuderi@ymail.com>
2023-03-07 21:22:01 +0100

    Add a big list of dependencies

---
+++ DemoAppWithSPM/DemoAppWithSPMApp.swift
@@ -0,0 +1,22 @@
+//
+//  DemoAppWithSPMApp.swift
+//  DemoAppWithSPM
+//
+//  Created by Andrea Scuderi on 07/03/2023.
+//
+
+import SwiftUI
+import Firebase
+import AWSS3
+import Yams
+import ChatSDK
+import Kingfisher
+
+@main
+struct DemoAppWithSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
